c4422ef4c14446689200669939b5f9b845efdcc8
dcreekp <dcreekp@gmail.com>
2017-08-16 16:19:52 +0100

    using moment as proxy for location of client browser

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import riot from 'riot'
+import moment from 'moment-timezone'
 import 'whatwg-fetch'
 import './tags/current-rates.tag'
 import './tags/select.tag'
 
+var zone = {
+  'Europe/London': 'GBP',
+  'Asia/Tokyo': 'JPY',
+  'Australia/Sydney': 'AUD',
+  'America/New_York': 'USD',
+  'Europe/Zurich': 'CHF',
+  'Europe/Berlin': 'EUR',
+}
 
-var currentRatesCallback = (currentRatesTag, base = 'GBP') => {
-  var url = location.protocol + '//' + location.host + '/api/oanda/' + base;
+var currentRatesCallback = (currentRatesTag, base = 'USD') => {
+  let url = location.protocol + '//' + location.host + '/api/oanda/' + base;
   fetch(url).then((response) => {
     return response.json();
   }).then((data) => {
@@ -13,4 +22,7 @@ var currentRatesCallback = (currentRatesTag, base = 'GBP') => {
   })
 }
 
-riot.mount('current-rates', {callback:currentRatesCallback})
+var tz = moment.tz.guess()
+var currency = zone[tz]
+
+riot.mount('current-rates', {callback:currentRatesCallback, currency:currency})
