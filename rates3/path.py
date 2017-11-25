8c0c3af8c7d900a8fc21948fcc5af1d77809c7d1
dcreekp <dcreekp@fukaz.at>
2017-11-25 17:47:43 +0000

    adjust path.py for nginx config

--- rates3/path.py
+++ rates3/path.py
@@ -2,10 +2,10 @@ from .app import App
 from .model import Oanda, Openx
 
 
-@App.path(model=Oanda, path='/oanda/{base}/')
+@App.path(model=Oanda, path='/api/oanda/{base}/')
 def get_oanda(base):
     return Oanda(base)
 
-@App.path(model=Openx, path='/xnepo/{base}/')
+@App.path(model=Openx, path='/api/xnepo/{base}/')
 def get_openx(base):
     return Openx(base)
