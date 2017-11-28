3e42c64e6ee984b18f07ce1f403022fc775e7cd1
dcreekp <dcreekp@fukaz.at>
2017-11-28 00:25:43 +0000

    some tests for openx model

--- rates3/tests/test_app.py
+++ rates3/tests/test_app.py
@@ -3,7 +3,9 @@ import rates3
 import pytest
 
 from webtest import TestApp as Client
+from decimal import Decimal
 from rates3 import model
+from rates3.tests.mock_data import OPENX_200
 
 
 @pytest.fixture
@@ -33,14 +35,32 @@ def openx_usd():
 def test_openx_api_latest(openx_usd):
     response = openx_usd.api_latest()
     assert response.status_code == 200
+    assert response.json()['rates'].keys() == OPENX_200['rates'].keys()
 
-def test_openx_rates(openx_usd):
-    rates = openx_usd.collect_rates()
-    assert type(rates) == dict
-
-def test_invert_base(openx_jpy):
+def test_openx_convert_rates():
     pass
 
+def test_openx_collect_quotes():
+    pass
+
+def test_openx_collect_rates(openx_usd):
+    rates = openx_usd.collect_rates()
+    assert isinstance(rates, dict)
+
+def test_openx_invert_base(openx_jpy):
+    assert openx_jpy.invert_base(OPENX_200['rates']) == Decimal('0.00900812')
+
+def test_openx_convert(openx_jpy):
+    assert openx_jpy.convert('3.7502', Decimal('0.00900812')) == '0.0337823'
+
+def test_openx_api_currency_list():
+    pass
+def test_openx_api_currency_list_wrong():
+    pass
+def test_openx_api_latest():
+    pass
+def test_openx_api_latest_wrong():
+    pass
 
 @pytest.fixture
 def oanda_jpy():
