e8a700de6c24e899a1080a7fc39e067cbeeaf16d
dcreekp <dcreekp@fukaz.at>
2017-11-30 16:42:51 +0000

    reorganise tests

---
+++ tests/__init__.py
@@ -0,0 +1,14 @@
+import morepath
+import rates3
+import pytest
+
+from webtest import TestApp as Client
+
+
+@pytest.fixture
+def client():
+    morepath.scan(rates3)
+    morepath.commit(rates3.App)
+    client = Client(rates3.App())
+    return client
+
--- tests/test_openx.py
+++ tests/test_openx.py
@@ -1,28 +1,15 @@
-import morepath
-import rates3
 import pytest
 
-from webtest import TestApp as Client
 from decimal import Decimal
 from rates3 import model
-from rates3.tests.mock_data import OPENX_200
 
+from .mock_data import OPENX_200
+from . import client
 
-@pytest.fixture
-def client():
-    morepath.scan(rates3)
-    morepath.commit(rates3.App)
-    client = Client(rates3.App())
-    return client
-
-def test_oanda(client):
-    response = client.get('/api/oanda/usd')
-    assert response.status_code == 200
 
 def test_openx(client):
     response = client.get('/api/xnepo/usd')
     assert response.status_code == 200
-
 
 @pytest.fixture
 def openx_jpy():
@@ -62,21 +49,3 @@ def test_openx_api_latest():
 def test_openx_api_latest_wrong():
     pass
 
-@pytest.fixture
-def oanda_jpy():
-    return model.Oanda('jpy')
-
-def test_oanda_model(oanda_jpy):
-    assert oanda_jpy.base == 'JPY'
-
-def test_oanda_collect_quotes(oanda_jpy):
-    quotes = oanda_jpy.quotes
-    assert type(quotes) == dict
-
-def test_oanda_base_rate(oanda_jpy):
-    assert str(oanda_jpy.base_rate('112.761', '112.894')) == '112.828'
-    assert str(oanda_jpy.base_rate('143.838', '143.913')) == '143.876'
-
-def test_oanda_quote_rate(oanda_jpy):
-    assert str(oanda_jpy.quote_rate('112.761', '112.894')) == '0.00886305'
-    assert str(oanda_jpy.quote_rate('143.838', '143.913')) == '0.00695043'
